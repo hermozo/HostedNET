cac6bec43aaca1ddc635f52fbd6129bc48dd8fcf
Angel Hermozo <mickyhermozo72@gmail.com>
2024-08-06 21:29:40 -0400

    Add project files.

---
+++ HostedNET/IntervalTaskHotedService.cs
@@ -0,0 +1,30 @@
+﻿namespace HostedNET
+{
+    public class IntervalTaskHotedService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(SaveFile, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+            return Task.CompletedTask;
+        }
+
+        public void SaveFile(object state)
+        {
+            string nameFIle = "a" + new Random().Next() + ".txt";
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Temp", nameFIle);
+            File.Create(path);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
---
+++ HostedNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HostedNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
